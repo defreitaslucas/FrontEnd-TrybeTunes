e0a52319e750e7b02931acc7be4ca506ad2b8692
LucasTapa <lucas.dfa@live.com>
2022-02-03 20:24:39 -0300

    requisito 3 e 4 100%

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,31 +1,44 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
-// import Carregando from './Carregando';
+import Carregando from './Carregando';
+import '../header.css';
 
 export default class Header extends Component {
   constructor() {
     super();
     this.state = {
       user: '',
-      // isLoading: false,
+      isLoading: true,
     };
   }
 
-  handleGetUser = async () => {
-    const { user } = this.state;
-    const result = await getUser();
-    console.log(result);
-    this.setState({ user: result });
-    return user;
+  async componentDidMount() {
+    const { name } = await getUser();
+    this.setState({ isLoading: false, user: name });
   }
 
   render() {
-    const { user } = this.state;
+    const { isLoading, user } = this.state;
     return (
-      <header data-testid="header-component">
-        <p data-testid="header-user-name">
-          {user}
-        </p>
+      <header data-testid="header-component" className="header">
+        {
+          isLoading ? <Carregando /> : (
+            <>
+              <h1>
+                TrybeTunes
+              </h1>
+              <p data-testid="header-user-name" className="user">
+                { user }
+              </p>
+              <nav className="nav">
+                <Link to="/search" data-testid="link-to-search">Search</Link>
+                <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+                <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+              </nav>
+            </>
+          )
+        }
       </header>
     );
   }
