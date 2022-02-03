864b07a25ef849cb968b0602a29ce2c385e71f0c
LucasTapa <lucas.dfa@live.com>
2022-02-03 15:29:41 -0300

    requisito 1 100%

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import Login from './Pages/Login';
+import Search from './Pages/Search';
+import Album from './Pages/Album';
+import Favorites from './Pages/Favorites';
+import Profile from './Pages/Profile';
+import ProfileEdit from './Pages/ProfileEdit';
+import NotFound from './Pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes by lucas</p>);
+    return (
+      <>
+        <p>TrybeTunes by Lucas</p>
+        <BrowserRouter>
+          <Login />
+          <Search />
+          <Album />
+          <Favorites />
+          <Profile />
+          <ProfileEdit />
+          <NotFound />
+        </BrowserRouter>
+      </>
+    );
   }
 }
 
---
+++ src/Pages/Favorites.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Favorites extends React.Component {
+  render() {
+    return (<div data-testid="page-favorites"> </div>);
+  }
+}
+
+export default Favorites;
